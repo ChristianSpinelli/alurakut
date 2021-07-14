3089c025a30fb72c514d4073d5181cabde979fa9
christian <c.spinelli93@gmail.com>
2021-07-14 10:35:23 -0300

    adicionado comunidades na página principal

--- pages/index.js
+++ pages/index.js
@@ -1,49 +1,34 @@
-import { Fragment, Component } from 'react';
+import React, { Fragment, useState } from 'react';
 import Box from '../src/components/Box';
 import MainGrid from '../src/components/Maingrid';
-import { AlurakutMenu, OrkutNostalgicIconSet } from '../src/lib/AlurakutCommons';
+import { AlurakutMenu, OrkutNostalgicIconSet, AlurakutProfileSidebarMenuDefault } from '../src/lib/AlurakutCommons';
 import { ProfileRelationsBoxWrapper } from '../src/components/ProfileRelations'
-import axios from 'axios';
 
 function ProfileSideBar(props){
   return(
-    <Box>
+    <Box as="aside">
       <img src={`https://github.com/${props.githubUser}.png`} style={{borderRadius:"8px"}}/>
+      <hr/>
+      <p>
+        <a className="boxLink" href={`https://github.com/${props.githubUser}`}>
+          @{props.githubUser}
+        </a>
+      </p>
+      <hr/>
+      <AlurakutProfileSidebarMenuDefault/>
     </Box>
   )
 }
 
-export default class Home extends Component {
-  constructor(props){
-    super(props)
-    this.state = {
-      githubUser:"ChristianSpinelli",
-      pessoas:[]
-    }
-
-    this.loadPessoas = this.loadPessoas.bind(this)
-  }
-
-  async componentDidMount(){
-    await this.loadPessoas()
-  }
-  
-  async loadPessoas(){
-    let  pessoas;
-    await axios.get(`https://api.github.com/users/${this.state.githubUser}/followers`)
-    .then((res)=>{
-      pessoas = res.data
-    })
-
-    this.setState({
-      ...this.state,
-      pessoas
-    })
-
-  }
-
-  render(){
-    const { githubUser, pessoas } = this.state
+export default function Home(){
+  const [ comunidades, setComunidades ] =  useState([{
+    id:new Date().toISOString(),
+    title:"Eu odeio acordar cedo", 
+    image:"https://alurakut.vercel.app/capa-comunidade-01.jpg"}
+  ])
+  const githubUser = "ChristianSpinelli"
+  const pessoas = [""]
+    
     return (
       <Fragment>
         <AlurakutMenu/>
@@ -58,20 +43,70 @@ export default class Home extends Component {
               </h1>
               <OrkutNostalgicIconSet confiavel={3} legal={3} sexy={3}/>
             </Box>
+            <Box>
+              <h2 className="subTitle">O que você deseja fazer?</h2>
+              <form onSubmit={(e) => { 
+                e.preventDefault()
+                const dadosForm = new FormData(e.target)
+                setComunidades([...comunidades, {
+                  id:new Date().toISOString(),
+                  title:dadosForm.get("title"), 
+                  image:dadosForm.get("image")
+                }])
+              }}>
+                <div>
+                  <input 
+                    placeholder="Qual vai ser o nome da sua comunidade?" 
+                    name="title" 
+                    aria-label="Qual vai ser o nome da sua comunidade?"
+                    type="text"
+                  />
+                </div>
+                <div>
+                  <input 
+                    placeholder="Coloque uma URL para usarmos de capa." 
+                    name="image" 
+                    aria-label="Coloque uma URL para usarmos de capa."
+                  />
+                </div>
+                <button>
+                  Criar Comunidade
+                </button>
+              </form>
+            </Box>
           </div>
           <div className="communityArea" style={{ gridArea:"communityArea"}}>
             <ProfileRelationsBoxWrapper>
               <h2 className="smallTitle">
-                Pessoas da Comunidade ({pessoas.length})
+                Meus Amigos ({pessoas.length})
               </h2>
               <ul>
               {pessoas.map((user)=>{
                 return(
                   <Fragment>
-                    <li>
-                    <a href={`/users/${user.login}`} key={user.login}>
-                      <img src={user.avatar_url}></img>
-                      <span>{user.login}</span>
+                    <li key={user.login}>
+                      <a href={`/users/${user.login}`}>
+                        <img src={user.avatar_url}></img>
+                        <span>{user.login}</span>
+                      </a>
+                    </li>
+                  </Fragment>
+                )
+              })}
+              </ul>
+            </ProfileRelationsBoxWrapper>
+            <ProfileRelationsBoxWrapper>
+              <h2 className="smallTitle">
+                Minhas Comunidades ({comunidades.length})
+              </h2>
+              <ul>
+              {comunidades.map((comunidade)=>{
+                return(
+                  <Fragment>
+                    <li key={comunidade.id}>
+                    <a href={`/comunidades/${comunidade.title}`}>
+                      <img src={comunidade.image}></img>
+                      <span>{comunidade.title}</span>
                     </a>
                     </li>
                   </Fragment>
@@ -79,13 +114,10 @@ export default class Home extends Component {
               })}
               </ul>
             </ProfileRelationsBoxWrapper>
-            <Box>
-              Comunidades
-            </Box>
           </div>    
         </MainGrid>
       </Fragment>
     )
-  }
+  
   
 }
